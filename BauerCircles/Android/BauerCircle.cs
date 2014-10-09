63aed4245f75e24f4c92ce60f84a350d450f5e9f
Basile-Emotum <basile@emotum.com>
2014-10-09 16:20:31 +1100

    - added "add a new circle when touched" - added "change color when tapped"

---
+++ BauerCircles/Android/BauerCircle.cs
@@ -0,0 +1,117 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using BauerCircles.Shared;
+using System.Threading.Tasks;
+
+namespace BauerCircles.Android
+{
+	public class BauerCircle : View, GestureDetector.IOnDoubleTapListener, GestureDetector.IOnGestureListener
+	{
+		public bool OnDown (MotionEvent e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool OnFling (MotionEvent e1, MotionEvent e2, float velocityX, float velocityY)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void OnLongPress (MotionEvent e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool OnScroll (MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void OnShowPress (MotionEvent e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public bool OnSingleTapUp (MotionEvent e)
+		{
+			return true;
+		}
+
+		bool GestureDetector.IOnDoubleTapListener.OnDoubleTap (MotionEvent e)
+		{
+			return true;
+		}
+
+		bool GestureDetector.IOnDoubleTapListener.OnDoubleTapEvent (MotionEvent e)
+		{
+			return true;
+		}
+
+		bool GestureDetector.IOnDoubleTapListener.OnSingleTapConfirmed (MotionEvent e)
+		{
+			throw new NotImplementedException ();
+		}
+
+		private GestureDetector _detector;
+
+		public BauerCircle (Context context) :
+			base (context)
+		{
+			Initialize ();
+		}
+
+		public BauerCircle (Context context, IAttributeSet attrs) :
+			base (context, attrs)
+		{
+			Initialize ();
+		}
+
+		public BauerCircle (Context context, IAttributeSet attrs, int defStyle) :
+			base (context, attrs, defStyle)
+		{
+			Initialize ();
+		}
+
+		void Initialize ()
+		{
+			this.SetBackgroundResource (Resource.Drawable.CircleDrawable);
+			_detector = new GestureDetector (this);
+			_detector.SetOnDoubleTapListener (this);
+
+			this.Touch+= HandleTouch;
+		}
+
+		void HandleTouch (object sender, TouchEventArgs e)
+		{
+			MotionEventActions action = e.Event.Action & MotionEventActions.Mask;
+
+			switch (action) {
+			case MotionEventActions.Up:
+				changeBackgroundColor ();
+				break;
+			default:
+				break;
+			}
+		}
+
+		private async Task changeBackgroundColor()
+		{
+			var color = await ColourLoverClient.GetNewRandomColorAsync ();
+
+			this.SetBackgroundColor (new global::Android.Graphics.Color (color.Rgb.Red, color.Rgb.Green, color.Rgb.Blue));
+		}
+
+	}
+}
+
--- BauerCircles/Android/MainActivity.cs
+++ BauerCircles/Android/MainActivity.cs
@@ -12,7 +12,7 @@ namespace BauerCircles.Android
 	[Activity (Label = "Android", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		private RelativeLayout _mainLayout;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,8 +21,39 @@ namespace BauerCircles.Android
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
+			_mainLayout = this.FindViewById<RelativeLayout> (Resource.Id.root_layout);
+			_mainLayout.Touch+= mainviewTouched;
 
+			addCircle (120, 120);
 		}
+
+		private void addCircle(int x, int y)
+		{
+			var newcircle = new BauerCircle (this);
+			var layoutparams = new RelativeLayout.LayoutParams (120, 120) {
+				LeftMargin = x - 60,
+				TopMargin = y - 60
+			};
+
+			_mainLayout.AddView (newcircle, layoutparams);
+		}
+
+		void mainviewTouched (object sender, View.TouchEventArgs e)
+		{
+			MotionEventActions action = e.Event.Action & MotionEventActions.Mask;
+
+			switch (action) {
+			case MotionEventActions.Up:
+				var x = e.Event.GetX ();
+				var y = e.Event.GetY ();
+				addCircle ((int)x, (int)y);
+
+				break;
+			default:
+				break;
+			}
+		}
+
 	}
 }
 
